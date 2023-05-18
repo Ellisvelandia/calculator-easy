13ba991e33c08a37a07e3af12720553dd38d00f9
Ellisvelandia <eyis619l@gmail.com>
2023-05-17 19:17:58 -0500

    calculator

---
+++ app.js
@@ -0,0 +1,25 @@
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+
+    if (buttonText == "*") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "AC") {
+      screenValue = "";
+      screen.value = screenValue;
+    } else if (buttonText == "=") {
+      screen.value = eval(screenValue);
+    } else if (buttonText == "Del") {
+      screen.value = screen.value.slice(0, -1);
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
